3c5e6e8e02a77ed769b2f2ddf1752086803eb012
BharatRamsf3693 <bharatram.h@syncfusion.com>
2025-07-09 08:51:08 +0530

    960496: Add the Stream Rendering with SfGrid sample

---
+++ Models/Order.cs
@@ -0,0 +1,11 @@
+﻿namespace Sample.Models
+{
+    public class Order
+    {
+        public int? OrderID { get; set; }
+        public string CustomerID { get; set; }
+        public string ShipCountry { get; set; }
+        public DateTime? OrderDate { get; set; }
+        public double? Freight { get; set; }
+    }
+}
---
+++ Services/OrderService.cs
@@ -0,0 +1,22 @@
+﻿using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Sample.Models;
+namespace Sample.Services
+{   
+    public class OrderService
+    {
+        private readonly HttpClient _httpClient;
+
+        public OrderService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<List<Order>> GetOrdersAsync()
+        {
+            return await _httpClient.GetFromJsonAsync<List<Order>>("https://blazor.syncfusion.com/services/production/api/Orders/");
+        }
+    }
+}
